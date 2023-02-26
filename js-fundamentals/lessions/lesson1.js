a212003133ee0e83dc25b79645f3d4aeb1d46436
AghaAmil <moravveji.amir.92@gmail.com>
2023-02-26 22:32:27 +0330

    intro, variables, constants, data types

--- js-fundamentals/lessions/lesson1.js
+++ js-fundamentals/lessions/lesson1.js
@@ -1,1 +1,30 @@
+// hello World
 console.log("Hello World!");
+
+// variables
+var firstName = "John";
+let lastname = "Smith";
+
+console.log(firstName);
+
+var age, dateOfBirth, sex;
+age = 29;
+sex = "male";
+
+console.log(age);
+
+age = 30;
+
+console.log(age);
+
+// constants
+const occupation = "QA Engineer";
+// occupation = "Front-End Developer"; // error
+console.log(occupation);
+
+// data types
+let middleName = 'David'; //string
+let ageOfBrother = 25; //number
+let isHeMarried = false; //boolean
+let yearsInMarriage = null; //null
+let numberOfCars = undefined; //undefined
