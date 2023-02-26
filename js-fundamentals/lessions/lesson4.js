042f655d2ea49e53f8eb37ec6aa0282ed7706f1e
AghaAmil <moravveji.amir.92@gmail.com>
2023-02-26 23:14:18 +0330

    relational and equality operators

---
+++ js-fundamentals/lessions/lesson4.js
@@ -0,0 +1,30 @@
+// renational or comparison operators
+
+// > - more than
+// < - less than
+// >= - more than equal
+// <= = less than equal
+
+let result = 10 > 5;
+console.log(result);
+
+result = 4 > 5;
+console.log(result);
+
+result = 5 < 5;
+console.log(result);
+
+result = 5 <= 5;
+console.log(result);
+
+result = 6 >= 5;
+console.log(result);
+
+//equalit operators
+let x = 1;
+console.log(x == '1'); // lose comparison
+console.log(x === '1'); // strick comparison
+
+
+
+
