286b862a7694962ea635a11f2af1ce0adf4e5826
AghaAmil <moravveji.amir.92@gmail.com>
2023-02-27 23:04:48 +0330

    functions part one

---
+++ js-fundamentals/lessions/lesson8.js
@@ -0,0 +1,23 @@
+// declerative fucntion
+function helloOne() {
+  console.log("Hello One!");
+}
+helloOne();
+helloOne();
+
+// anoymus function
+let helloTow = function () {
+  console.log("Hello Two!");
+};
+helloTow();
+
+//ES6 function syntax or arrow function
+let helloThree = () => {
+  console.log("Hello Three!");
+};
+helloThree();
+
+//function with aguments
+let printName = (name) {
+  console.log()
+}
