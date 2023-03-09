215eeae0bf743909a60454dbb81f944d265166dd
AghaAmil <moravveji.amir.92@gmail.com>
2023-03-09 16:26:02 +0330

    function part two

--- js-fundamentals/lessions/lesson8.js
+++ js-fundamentals/lessions/lesson8.js
@@ -5,11 +5,11 @@ function helloOne() {
 helloOne();
 helloOne();
 
-// anoymus function
-let helloTow = function () {
+// anonymous function
+let helloTwo = function () {
   console.log("Hello Two!");
 };
-helloTow();
+helloTwo();
 
 //ES6 function syntax or arrow function
 let helloThree = () => {
@@ -18,6 +18,15 @@ let helloThree = () => {
 helloThree();
 
 //function with aguments
-let printName = (name) {
-  console.log()
-}
+let printName = (name, lastname) => {
+  console.log(`Welcome to our course, ${name} ${lastname}.`);
+};
+printName("Amir", "Moravveji");
+printName("Mike", "Tyson");
+
+//function with return
+let multiplyByTwo = (number) => {
+  return (number *= 2);
+};
+
+console.log(multiplyByTwo(2));
